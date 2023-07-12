23c632127b00036967cfde614f78b3331d2e89bf
Gnanesh <102707515+kirangnanesh@users.noreply.github.com>
2023-07-11 21:34:08 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import ExpenseItem from "./components/ExpenseItem";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
